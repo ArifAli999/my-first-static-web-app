f5e397bb9d645e705fc6ce1d5c6f8636458f7d93
ArifAli999 <82163631+ArifAli999@users.noreply.github.com>
2022-03-01 03:22:54 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import BoxScore from './components/boxscore';
+import UpcomingGames from './components/upcomingGames';
+import Header from './components/header';
+import TestData from './components/testcomp';
+import 'antd/dist/antd.css';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <Header/>
+
+      <Routes>
+          
+          <Route path="boxscore" element={<BoxScore/>} />
+          <Route path="upcomingGames" element={<UpcomingGames/>} />
+          <Route path="testcomp" element={<TestData/>} />
+          </Routes>
+
+
+
+     
+       
+
+        
+      </div>
+ 
+
+      </BrowserRouter>
+
+
+
+
+
+  );
 }
 
 export default App;
